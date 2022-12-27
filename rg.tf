b6e5eb73cb301b23d4a415aff134226c47d33275
Kvs Vishnu Kumar <36696407+Vishnukvsvk@users.noreply.github.com>
2022-12-28 00:14:59 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    kubernetes = { 
+
+      source  = "hashicorp/kubernetes" 
+
+    } 
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "r1" {
   name     = "g123"
   location = "West Us"
